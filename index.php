f3cf8664a4503202a5236d2dc0debf8227b400ed
Md. Raihanul Islam Rafi <r.rafi.aiub@gmail.com>
2022-03-05 21:09:26 +0600

    Layered architecture

---
+++ index.php
@@ -0,0 +1,35 @@
+<html>
+<head>
+	<title>Public Home</title>
+</head>
+<style>
+	h1,h4 
+	{
+		display: inline-block;
+	}
+	p.ex1 
+	{
+		padding-left: 380%;
+	}
+</style>
+<body>
+	<table>
+		<table border="1" width="100%">
+		<tr>
+			<td>
+				 <pre><h1 style=font-size:25px> X Company</h1><h4><p class="ex1"><a href="publicHome.php">Public Home</a> | <a href="login.php">Login</a> | <a href="registration.php">Registration</a></pre></p></h4>
+			</td>
+		</tr>
+		<tr height="310px">
+			<td>
+				<h1>Welcome to XCompany</h1>
+			</td>
+		</tr>
+		<tr>
+			<td>
+				<h3><center>Copyright &copy; 2017</center></h3>
+			</td>
+		</tr>
+	</table>
+</body>
+</html>

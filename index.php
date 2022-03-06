119f8c56bb57d66106ea7da8b4695e7edc2d528d
Md. Raihanul Islam Rafi <r.rafi.aiub@gmail.com>
2022-03-07 00:53:14 +0600

    404

--- index.php
+++ index.php
@@ -1,50 +1,43 @@
-
 <html>
-<head>
-	<title>Public Home  .... test 02</title>
-</head>
-<style>
-	h1,h4 
-	{
-		display: inline-block;
-	}
-	p.ex1 
-	{
-		padding-left: 380%;
-	}
-</style>
 <body>
-	<table>
-		<table border="1" width="100%">
-		<tr>
-			<td>
-				 <pre><h1 style=font-size:25px> X Company</h1><h4><p class="ex1"><a href="publicHome.php">Public Home</a> | <a href="login.php">Login</a> | <a href="registration.php">Registration</a></pre></p></h4>
-			</td>
-		</tr>
-		<tr height="310px">
-			<td>
-				<h1>Welcome to XCompany</h1>
-			<form  method="POST" action="loginPath.php">
-			<select name="login">
-			<option value="m">Manager</option>
-			<option value="w">Worker</option>
-			<option value="s">Seller</option>
-			<option value="b">Buyer</option>
-			
-		</select>
-		<br>
-		<hr style="width:9%;text-align:left;margin-left:2"></hr>
-		<input type="submit" name="loginSubmit" value="Submit">
+<title> Home Page </title>
+<table border = "2" width=100%>
+    <tr>
+        <td><center><h1 style=font-size:50px>Germents Function</h1></center></td>
+    </tr>
+</table>
 
+<h1><center><p style="font-size: 150px;">Welcome</p></center></h1>
+
+
+<center>
+	<form  method="POST" action="login.php">
+	<input type="submit" name="login" value="Login" style="height:150px; width:300px">
 	</form>
-				
-			</td>
-		</tr>
-		<tr>
-			<td>
-				<h3><center>Copyright &copy; 2017</center></h3>
-			</td>
-		</tr>
-	</table>
+    
+</center>
+
+
+<br><br><br><br><br><br><br><br><br><br><br><br>
+<table border = "2" width=100%>
+    <tr rowspan="4">
+        <td>
+        <p style="font-size: 40px;">Get in Touch</p</td>
+        <td rowspan="2">
+        <center>
+            <p style="font-size: 40px;">
+            <pre><p style="font-size: 20px;"><a href="#"><b>Product</b></a></p></pre>
+            <pre><p style="font-size: 20px;"><a href="#"><b>Service</b></a></p></pre>
+            <pre><p style="font-size: 20px;"><a href="#"><b>Contact Us</b></a></p></pre>
+            <pre><p style="font-size: 20px;"><a href="#"><b>About Us</b></a></p></pre>
+            </p>
+        </center>
+         </td>
+    </tr>
+    <tr>
+        <td><p style="font-size: 40px;"><a href="#">Facebook</a> <a href="#">Twitter</a>  <a href="#">Instagram</a></p></td>
+    </tr>
+</table>
+
 </body>
 </html>
---
+++ login.php
@@ -0,0 +1,40 @@
+<html>
+<head>
+	<title>Public Home  .... test 02</title>
+
+<body>
+	<table>
+		<table border="1" width="100%">
+		<tr>
+			<td>
+			</td>
+		</tr>
+		<tr height="310px">
+			<td> 
+			<h1>Welcome to XCompany</h1>
+			<form  method="POST" action="loginPath.php">
+            Log in as : 
+			<select name="login">
+			<option value="m">Manager</option>
+			<option value="w">Worker</option>
+			<option value="s">Seller</option>
+			<option value="b">Buyer</option>
+			
+		</select>
+		<br>
+		<hr style="width:9%;text-align:left;margin-left:2"></hr>
+        
+		<input type="submit" name="loginSubmit" value="Login">
+
+	</form>
+				
+			</td>
+		</tr>
+		<tr>
+			<td>
+				<h3><center>Copyright &copy; 2017</center></h3>
+			</td>
+		</tr>
+	</table>
+</body>
+</html>

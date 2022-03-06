f7a497900b26f913e7fd7410688bc6b0e66a5d74
mushi <mushfiq1420@gmail.com>
2022-03-07 02:34:40 +0600

    Log in page view

--- Company/views/login.php
+++ Company/views/login.php
@@ -1,40 +1,45 @@
 <html>
-<head>
-	<title>Public Home  .... test 02</title>
-
 <body>
-	<table>
-		<table border="1" width="100%">
-		<tr>
-			<td>
-			</td>
-		</tr>
-		<tr height="310px">
-			<td> 
-			<h1>Welcome to XCompany</h1>
-			<form  method="POST" action="../controllers/loginPath.php">
-            Log in as : 
-			<select name="login">
-			<option value="m">Manager</option>
-			<option value="w">Worker</option>
-			<option value="s">Seller</option>
-			<option value="b">Buyer</option>
-			
-		</select>
-		<br>
-		<hr style="width:9%;text-align:left;margin-left:2"></hr>
-        
-		<input type="submit" name="loginSubmit" value="Login">
+<title> Home Page </title>
+<table border = "2" width=100%>
+    <tr>
+        <td><center><h1 style=font-size:50px>Germents Function</h1></center></td>
+    </tr>
+</table>
 
-	</form>
-				
-			</td>
-		</tr>
-		<tr>
-			<td>
-				<h3><center>Copyright &copy; 2017</center></h3>
-			</td>
-		</tr>
-	</table>
+<style>
+    div {
+      background-color: gainsboro;
+      width: 300px;
+      border: 15px black;
+      padding: 50px;
+      margin: 20px;
+      width: 91%;
+    }
+    </style>
+<div > 
+  
+    <form  method="POST" action="../controllers/loginPath.php">
+    <center>
+        <p style="font-size: 40px;">Log in as : </p>
+        <select name="login">
+        <option value="m">Manager</option>
+        <option value="w">Worker</option>
+        <option value="s">Seller</option>
+        <option value="b">Buyer</option>
+    </center>
+    </select>
+    <br>
+    <br>
+    <br>
+    
+    <input type="submit" name="loginSubmit" value="Login">
+</form>
+
+</div>
+
+
+    </center>
+</div>
 </body>
 </html>

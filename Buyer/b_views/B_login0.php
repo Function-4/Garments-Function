c34928e348fdacef54baff1a6de07faebe216c90
mushi <mushfiq1420@gmail.com>
2022-03-07 13:35:10 +0600

    B_req & B_login

---
+++ Buyer/b_views/B_login0.php
@@ -0,0 +1,24 @@
+<html>
+    <body>
+    <title> Home Page </title>
+    <table border = "2" width=100%>
+        <tr>
+            <td><h1 style=font-size:50px >Welcome to Germents Function ,</h1></td>
+        </tr>
+    </table>
+    <center><p style="font-size: 50px;"><b>Log in as Buyer</b></p></center>
+
+
+        <form>
+    
+        <center><p style="font-size: 30px;">User Name</p> <input type="text" name="Name" value="" /></center>
+        <center><p style="font-size: 30px;">Password</p> <input type="text" name="Name" value="" /></center>
+        <br>
+        <br>
+     
+       <center> <input type="submit" name="submit" value="Submit" style="height:50px; width:100px"> </center>
+        </form>
+
+
+    </body>
+</html>
---
+++ Buyer/b_views/Reg.php
@@ -0,0 +1,36 @@
+<html>
+    <body>
+    <title> Home Page </title>
+    <table border = "2" width=100%>
+        <tr>
+            <td><h1 style=font-size:50px >Welcome to Germents Function,</h1></td>
+        </tr>
+    </table>
+    <center><p style="font-size: 30px;"><b>Sign Up</b></p></center>
+
+    <form>
+        <center>
+        <table>
+
+        <center><p style="font-size: 30px;">First Name</p> <input type="text" name="Name" value="" /></center>
+            
+
+        <center><p style="font-size: 30px;">User Name </p> <input type="text" name="Name" value="" /></center>
+
+
+        <center><p style="font-size: 30px;">Password</p> <input type="text" name="Name" value="" /></center>
+
+
+        <center><p style="font-size: 30px;"> Email</p> <input type="text" name="Name" value="" /></center>
+
+
+        <center><p style="font-size: 30px;">Phone no.</p> <input type="text" name="Name" value="" /></center>
+
+        </table> 
+    </center>
+    <br><br>
+    <br>
+      <center>  <input type="submit" name="submit" value="Submit" style="height:50px; width:100px"> </center>
+</form>
+</body>
+</html>          

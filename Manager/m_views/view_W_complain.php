f7b751d7335b7bddfd1893ab51b746b377dffd23
Md. Raihanul Islam Rafi <r.rafi.aiub@gmail.com>
2022-03-12 21:49:21 +0600

    com w

--- Manager/m_views/view_W_complain.php
+++ Manager/m_views/view_W_complain.php
@@ -1,7 +1,37 @@
 <?php
 require('../m_controllers/m_header.php');
-$url = '../../Worker/w_models/w_complain.txt';
-$file = fopen($url, 'r');
-echo fread($file , filesize($url));
-fclose($file);
 ?>
+<html>
+<head>
+	<title>View complain</title>
+</head>
+<body>
+        <table border="1">
+			<tr>
+                <td><center><b>Workers Complain</b></center></td>
+            </tr>
+
+			<?php 
+				$file = fopen('../../Worker/w_models/w_complain.txt', 'r');
+				
+				while (!feof($file)) 
+                {
+					$user = fgets($file);
+					if($user == null)
+                    {
+						break;
+					}
+			?>
+
+			<tr>
+				<td><pre><?=$user?></pre></td>
+			</tr>
+
+			<?php
+				}
+			?>
+		</table>
+		</fieldset>
+	</form>
+</body>
+</html>
--- Worker/w_views/w_Home.php
+++ Worker/w_views/w_Home.php
@@ -8,6 +8,7 @@
 <body>
 	<pre><h1>Welcome home !  <?=$_SESSION['current_worker'][0]?> Worker</h1></pre> 
 	<br><br>
+		<a href="w_complain.php">Complain to Manager</a> <br>
 		<a href="../w_controllers/w_logout.php">Logout</a>
 </body>
 </html>

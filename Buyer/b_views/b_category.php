0fc877f79505f353f614d924a0845337ab56650c
mushi <mushfiq1420@gmail.com>
2022-03-13 20:06:33 +0600

    Buyers

--- Buyer/b_views/b_category.php
+++ Buyer/b_views/b_category.php
@@ -1,11 +1,6 @@
-<?php 
-    require('../b_controllers/b_header.php');
-?>
-
 <html>
 <body>
 <title> Feedback </title>
-
 <table border = "2" width=100%>  
     <tr>
         <td><h1 style=font-size:50px><center>Germents Function</center></h1></td>
@@ -13,7 +8,7 @@
         <td><center><p style=font-size:30px><a href="#"> Oder Product</a> </p></center></td>
         <td><center><p style=font-size:30px><a href="#"> Complain</a> </p></center></td>
         <td><center><p style=font-size:30px><a href="#"> Request for Product</a></p></center></td>
-		<td><center><p style=font-size:30px><a href="../b_controllers/b_logout.php">Logout</a></center></td>
+        <td><center><p style=font-size:30px><a href="../b_controllers/b_logout.php">Logout</a></center></td>
     </tr>
 </table>
 
---
+++ Buyer/b_views/b_order_product.php
@@ -0,0 +1,44 @@
+<html>
+<body>
+<title> Feedback </title>
+<table border = "2" width=100%>  
+    <tr>
+        <td><h1 style=font-size:50px><center>Germents Function</center></h1></td>
+        <td><center><p style=font-size:30px><a href="#"> Product</a> </p></center></td>
+        <td><center><p style=font-size:30px><a href="#"> Oder Product</a> </p></center></td>
+        <td><center><p style=font-size:30px><a href="#"> Complain</a> </p></center></td>
+        <td><center><p style=font-size:30px><a href="#"> Request for Product</a></p></center></td>
+        <td><center><p style=font-size:30px><a href="../b_controllers/b_logout.php">Logout</a></center></td>
+    </tr>
+</table>
+
+<form action="" method="post">
+    
+<center>
+        <p style=font-size:30px><h1><u>Oder of the product</u></h1></p>    
+            <p style=font-size:30px>Username</p>
+            <input type="text" name="username" value="">
+
+            <p style=font-size:30px>Item Name</p>
+            <input type="text" name="Itemname" value="">
+
+            <p style=font-size:30px>Quantity</p>
+            <input type="text" name="quantity" value="">
+
+            <p style=font-size:30px>Color</p>
+            <input type="text" name="color" value="">
+
+            <p style=font-size:30px>Size</p>
+            <input type="text" name="size" value="">
+
+
+            <br><br><br>
+<center> <input type="submit" name="bLogin" value="Submit" style="height:50px; width:100px"> </center>
+
+                    
+</center>
+
+</form>
+
+</body>
+</html>

8d764c4efac8affe77f0d8e74a51ec137906a5d6
Your Name <mushfiq1420@gmail.com>
2022-04-03 21:31:56 +0600

    BuyerCSS

--- views/b_req.php
+++ views/b_req.php
@@ -1,50 +1,60 @@
 <html>
 
 <body>
-    <title> Home Page </title>
-    <table border="2" width=100%>
-        <tr>
-            <td>
-                <h1 style=font-size:50px>Welcome to Germents Function,</h1>
-            </td>
-        </tr>
-    </table>
-    <center>
-        <p style="font-size: 30px;"><b>Sign Up</b></p>
-    </center>
+    <title> Buyer Registration </title>
+
+
+    <p3> 
+    <center>Welcome to Buyer Registration  Page </center>
+    </p3>
+
 
     <form method="POST" action="../controllers/regCheck.php">
         <center>
             <table>
-
+            <tr>
+                <td>
                 <center>
                     <p style="font-size: 30px;">First Name</p> <input type="text" name="1name" value="" />
                 </center>
+                </td>
 
+                <td>
                 <center>
                     <p style="font-size: 30px;">Last Name</p> <input type="text" name="2name" value="" />
                 </center>
+                </td>
+
+            </tr>
 
 
+            <tr>
+            <td>
                 <center>
                     <p style="font-size: 30px;">User Name </p> <input type="text" name="username" value="" />
                 </center>
+            </td>
+
+                <td>
+                        <center>
+                            <p style="font-size: 30px;">Password</p> <input type="password" name="password" value="" />
+                        </center>
+                </td>
+            </tr>
 
 
-                <center>
-                    <p style="font-size: 30px;">Password</p> <input type="password" name="password" value="" />
-                </center>
-
-
-                <center>
-                    <p style="font-size: 30px;"> Email</p> <input type="email" name="email" value="" />
-                </center>
-
-
-                <center>
-                    <p style="font-size: 30px;">Phone no.</p> <input type="number" name="contact" value="" />
-                </center>
-
+            <tr>
+                    <td>
+                        <center>
+                            <p style="font-size: 30px;"> Email</p> <input type="email" name="email" value="" />
+                        </center>
+                    </td>
+                    <td>
+                        <center>
+                            <p style="font-size: 30px;">Phone no.</p> <input type="number" name="contact" value="" />
+                        </center>
+                    </td>
+            </tr>
             </table>
         </center>
         <br><br>

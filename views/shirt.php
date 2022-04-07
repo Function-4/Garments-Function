85ad2d28aa4f6862b02561b82b83051532826163
Your Name <mushfiq1420@gmail.com>
2022-04-07 20:06:17 +0600

    css01

--- views/shirt.php
+++ views/shirt.php
@@ -1,5 +1,117 @@
 <html>
     <body>
-        This is shirt Page
+    <title> Product </title>
+    <link rel="stylesheet" href="../CSS/b_login.css">
+<ul>
+  <li><a href="b_home.php">Home</a></li>
+  <li><a href="b_product.php">Product</a></li>
+  <li><a href="b_order.php">Oder Product</a></li>
+  <li><a href="b_complain.php">Complain</a></li>
+  <li><a href="b_Feedback.php">Feedback</a></li>
+  <li><a href="b_reqforproduct.php">Request for Product</a></li>
+  <li><form method="POST" action="../controllers/logout.php">
+            		<input type="submit" name="b_logout" value="Logout" style="height:60px; width:100px"  >
+        			</form>
+                </li>
+</ul>
+
+
+
+<br>
+<br>
+
+<center><p4>Shirt Product</p4></center>
+
+<br>
+<br>
+<table width="100%">
+<tr>
+
+
+<td height="100%">
+<a href="b_order.php">
+        <div class="card" >
+            <img src="../assets/1.jpg" alt="peojuct" style="width:100%">
+                <div class="container">
+                   <center> <h4><b>shirt1</b></h4></center>
+                    <center><p>Order Now</p> </center>
+                </div>
+        </div>
+</a> 
+    </td>
+
+    <td> 
+        <a href="b_order.php">
+        <div class="card" >
+            <img src="../assets/1.jpg" alt="peojuct" style="width:100%">
+                <div class="container">
+                   <center> <h4><b>Shirt0</b></h4></center>
+                   <center><p>Order Now</p> </center>
+                </div>
+        </div>
+        </a>        
+    </td>
+
+
+    <td height="100%">
+    <a href="b_order.php">
+        <div class="card" >
+            <img src="../assets/1.jpg" alt="peojuct" style="width:100%">
+                <div class="container">
+                   <center> <h4><b>Shirt2</b></h4></center>
+                   <center><p>Order Now</p> </center>
+                </div>
+        </div>
+</a>
+    </td>
+
+</tr>
+
+<tr>
+
+<td height="100%">
+<a href="b_order.php">
+        <div class="card" >
+            <img src="../assets/1.jpg" alt="peojuct" style="width:100%">
+                <div class="container">
+                   <center> <h4><b>Shirt3</b></h4></center>
+                   <center><p>Order Now</p> </center>
+                </div>
+        </div>
+</a>
+    </td>
+
+    <td>
+    <a href="b_order.php">
+        <div class="card" >
+            <img src="../assets/1.jpg" alt="peojuct" style="width:100%">
+                <div class="container">
+                   <center> <h4><b>Shirt4</b></h4></center>
+                   <center><p>Order Now</p> </center>
+                </div>
+        </div>
+</a>
+    </td>
+
+
+    <td height="100%">
+    <a href="b_order.php">
+        <div class="card" >
+            <img src="../assets/1.jpg" alt="peojuct" style="width:100%">
+                <div class="container">
+                   <center><h4><b>Shirt5</b></h4></center>
+                   <center><p>Order Now</p> </center>
+                </div>
+        </div>
+    </td>
+</a>
+
+</tr>
+
+</table>
+<br>
+
+
+
     </body>
 </html>
